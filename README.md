07d444c6993a4354ebadced4dceecea44a9e3c36
abc-abc-ab <152158429+abc-abc-ab@users.noreply.github.com>
2024-06-02 18:04:07 +0900

    upload!

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+---
+
+---
 # marui
 ## These project
 these projects are marui's `codepen.io` projects.
--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-let elm = (x) = {
+let i = 1;
+
+window.onload = () => {
+  elm("#bOne").addEventListener("click", () => {
+    elm("#output").textContent += "output";
+    i++;
+  })
+}
+
+/** 
+ * @param {string} x */
+function elm(x){
   return x.includes("#")?
     document.querySelector(x):
     document.querySelectorAll(x)
-}
-window.onload = () = {
-  elm("#bOne").addEventListener("click",{
-    elm("#output").textContent += "Output!!";
-  }
 }
