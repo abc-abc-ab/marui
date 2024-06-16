18431557119e4eac2ebb143f23128a23ed1d4807
abc-abc-ab <152158429+abc-abc-ab@users.noreply.github.com>
2024-06-16 15:07:56 +0900

    Update

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let i = 1;
 
 window.onload = () => {
+  elm("#output").innerHTML = localStorage.getItem("output");
   elm("#bOne").addEventListener("click", () => {
     elm("#output").innerHTML += `Output ${i}<br>`;
     i++;
+    localStorage.setItem("output", elm("#output").innerHTML)
   })
 }
 
